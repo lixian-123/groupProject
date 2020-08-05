5f1109eb211e6054accfec0177316835a201561d
lx <lx@163.com>
2020-08-05 19:40:42 +0800

    ceshi  lixian11

--- tg-parent/tg-eureka/src/main/java/com/kgc/EurekaServerApp.java
+++ tg-parent/tg-eureka/src/main/java/com/kgc/EurekaServerApp.java
@@ -9,7 +9,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 public class EurekaServerApp {
     public static void main(String[] args) {
         //启动eureka
-        //李贤 更新
+        //李贤 更新1111
         SpringApplication.run(EurekaServerApp.class,args);
     }
 }
