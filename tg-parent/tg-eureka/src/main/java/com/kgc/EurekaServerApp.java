8e76cae34bed3614116f841a4765c72dc2d7a877
lx <lx@163.com>
2020-08-07 16:01:28 +0800

    lx  8-7  22

--- tg-parent/tg-eureka/src/main/java/com/kgc/EurekaServerApp.java
+++ tg-parent/tg-eureka/src/main/java/com/kgc/EurekaServerApp.java
@@ -1,15 +1,15 @@
 package com.kgc;
 
-        import org.springframework.boot.SpringApplication;
-        import org.springframework.boot.autoconfigure.SpringBootApplication;
-        import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
 public class EurekaServerApp {
     public static void main(String[] args) {
         //启动eureka
-        //李贤
+        //李贤 
         //李思广 更新
         //黄学文 更新
         //张可 更新1111
