2068e0e014815e8ab00c9bfd6080f75600322c89
xm <xm@126.com>
2020-08-14 09:36:23 +0800

    hxw update

--- tg-parent/tg-leader-provider/src/main/java/com/kgc/service/RestLeaderService.java
+++ tg-parent/tg-leader-provider/src/main/java/com/kgc/service/RestLeaderService.java
@@ -2,6 +2,7 @@ package com.kgc.service;
 
 import com.alibaba.fastjson.JSON;
 import com.kgc.pojo.order.TeamOrder;
+import com.kgc.util.DateUtil;
 import com.kgc.util.RedisUtils;
 import nl.bitwalker.useragentutils.UserAgent;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -12,15 +13,24 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
+import java.text.ParseException;
+import java.util.Date;
 
 @RestController
 public class RestLeaderService {
     @Resource
     RedisUtils redisUtils;
     @RequestMapping("/saveTeamRedis")
-    public boolean saveTeamRedis(@RequestBody TeamOrder teamOrder){
-        String key="TeamGoods_"+teamOrder.getTeamId();
-        return redisUtils.set(key, JSON.toJSONString(teamOrder));
+    public boolean saveTeamRedis(@RequestBody TeamOrder teamOrder) throws ParseException {
+        String key="TeamGoods_"+teamOrder.getGoodsId();
+        Date startTime= DateUtil.parse(teamOrder.getStartTime(),"yyyy-MM-dd hh:mm:ss");
+        Date endTime=DateUtil.parse(teamOrder.getEndTime(),"yyyy-MM-dd hh:mm:ss");
+        long betweenTime=DateUtil.getSecond(startTime,endTime);
+        if(redisUtils.set(key, JSON.toJSONString(teamOrder))){
+            return redisUtils.expire(key,betweenTime);
+        }
+        return false;
+
     }
 
 }
