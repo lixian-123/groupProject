6beb3e38bef46e785bb1b0e410b409c6c9e5d394
xm <xm@126.com>
2020-09-04 09:05:02 +0800

    hxw update

--- tg-parent/tg-commen/src/main/java/com/kgc/config/ElastricSearchConfig.java
+++ tg-parent/tg-commen/src/main/java/com/kgc/config/ElastricSearchConfig.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ElastricSearchConfig {
-    //ceshi
+    //
     @Bean
     public RestHighLevelClient restHighLevelClient(){
         RestHighLevelClient client = new RestHighLevelClient(
